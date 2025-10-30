927f6450dfe7cccd1cd566babcd0b8acf6be4132
isakgranqvist2021 <isakwebdev@gmail.com>
2025-10-30 20:13:04 +0100

    cmt

--- app/app/form/[id]/page.tsx
+++ app/app/form/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { MainContainer } from 'containers/main-container';
+import { sectionMapper } from 'db/mapper';
 import { findFormById, listSectionsByFormId } from 'db/query';
 import { PageProps } from 'types/page';
 
@@ -20,15 +21,31 @@ export default async function Page(props: PageProps<{ id: string }>) {
   }
 
   const sections = await listSectionsByFormId(form.id);
+  const mappedSections = sections.map(sectionMapper);
 
   return (
     <MainContainer>
       <h1>{form.title}</h1>
       <p className="whitespace-pre-wrap">{form.description}</p>
 
-      {sections.map((section) => (
-        <p key={section.id}>{section.title}</p>
-      ))}
+      {mappedSections.map((section) => {
+        switch (section.type) {
+          case 'text':
+            return (
+              <section key={section.id}>
+                <h2 className="font-bold mb-2">{section.title}</h2>
+
+                <input
+                  required={section.required}
+                  key={section.id}
+                  className="input input-bordered w-full mb-4"
+                />
+              </section>
+            );
+        }
+      })}
+
+      <button className="btn btn-primary">Submit</button>
     </MainContainer>
   );
 }
