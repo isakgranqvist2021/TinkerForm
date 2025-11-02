fd711e633e8bfa660bd7030a02321d09cc561cd9
isakgranqvist2021 <contact@granqvist.dev>
2025-11-02 21:58:07 +0100

    aaa

--- app/components/sections-list.tsx
+++ app/components/sections-list.tsx
@@ -86,6 +86,7 @@ export function SectionsList() {
     fieldArray.fields.forEach((section, index) => {
       fieldArray.update(index, { ...section, index });
     });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [fieldArray.fields.length]);
 
   return (
