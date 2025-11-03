33e1791e9b4069bb742437836ddcbc57b66af4c1
isakgranqvist2021 <isakwebdev@gmail.com>
2025-11-03 15:24:14 +0100

    test

--- app/models/answer-form.test.ts
+++ app/models/answer-form.test.ts
@@ -3,22 +3,6 @@ import * as clientSchema from './answer-form';
 import * as serverSchema from './answer-form';
 import { Section } from './form';
 
-test('getSchema (optional with empty string)', () => {
-  const payload: Section = {
-    description: 'Please provide your feedback.',
-    id: 'section-1',
-    index: 0,
-    max: 500,
-    min: 10,
-    required: false,
-    title: 'Feedback',
-    type: 'text',
-  };
-
-  expect(clientSchema.getSchema(payload).safeParse('').error).toBeUndefined();
-  expect(serverSchema.getSchema(payload).safeParse('').error).toBeUndefined();
-});
-
 test('getSchema (optinal with empty string)', () => {
   const payload: Section = {
     type: 'text',
@@ -85,4 +69,20 @@ test('getSchema (required with without string)', () => {
 
   expect(clientSchema.getSchema(payload).safeParse('').error).toBeDefined();
   expect(serverSchema.getSchema(payload).safeParse('').error).toBeDefined();
+});
+
+test('getSchema (required with without long enough string)', () => {
+  const payload: Section = {
+    type: 'text',
+    description: 'Please explain your reason for applying',
+    id: 'e6d8e732-fdf2-494c-b9d0-1fb333c56620',
+    index: 1,
+    required: true,
+    title: 'What is your reason for applying?',
+    min: 10,
+    max: 5000,
+  };
+
+  expect(clientSchema.getSchema(payload).safeParse('abc').error).toBeDefined();
+  expect(serverSchema.getSchema(payload).safeParse('abc').error).toBeDefined();
 });
