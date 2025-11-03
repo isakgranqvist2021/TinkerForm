cb800846d282c17dc782b227817945feb3869849
isakgranqvist2021 <contact@granqvist.dev>
2025-11-03 21:47:30 +0100

    Add pricing stuff

--- app/app/pricing/page.tsx
+++ app/app/pricing/page.tsx
@@ -3,9 +3,136 @@ import { Drawer } from 'components/drawer';
 export default function Page() {
   return (
     <Drawer>
-      <div className="max-w-5xl mx-auto w-full p-4">
-        <h1>Pricing Page</h1>
+      <div className="max-w-5xl mx-auto w-full px-4 py-12 flex flex-col gap-10">
+        <h1 className="text-4xl font-bold text-center">Pricing</h1>
+
+        <div className="flex gap-2 flex-wrap">
+          <PricingCard />
+          <PricingCard />
+          <PricingCard />
+        </div>
       </div>
     </Drawer>
+  );
+}
+
+function PricingCard() {
+  return (
+    <div className="card flex-grow bg-base-100 shadow-sm">
+      <div className="card-body">
+        <span className="badge badge-xs badge-warning">Most Popular</span>
+        <div className="flex justify-between">
+          <h2 className="text-3xl font-bold">Premium</h2>
+          <span className="text-xl">$29/mo</span>
+        </div>
+        <ul className="mt-6 flex flex-col gap-2 text-xs">
+          <li>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="size-4 me-2 inline-block text-success"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5 13l4 4L19 7"
+              />
+            </svg>
+            <span>High-resolution image generation</span>
+          </li>
+          <li>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="size-4 me-2 inline-block text-success"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5 13l4 4L19 7"
+              />
+            </svg>
+            <span>Customizable style templates</span>
+          </li>
+          <li>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="size-4 me-2 inline-block text-success"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5 13l4 4L19 7"
+              />
+            </svg>
+            <span>Batch processing capabilities</span>
+          </li>
+          <li>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="size-4 me-2 inline-block text-success"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5 13l4 4L19 7"
+              />
+            </svg>
+            <span>AI-driven image enhancements</span>
+          </li>
+          <li className="opacity-50">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="size-4 me-2 inline-block text-base-content/50"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5 13l4 4L19 7"
+              />
+            </svg>
+            <span className="line-through">Seamless cloud integration</span>
+          </li>
+          <li className="opacity-50">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="size-4 me-2 inline-block text-base-content/50"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5 13l4 4L19 7"
+              />
+            </svg>
+            <span className="line-through">Real-time collaboration tools</span>
+          </li>
+        </ul>
+        <div className="mt-6">
+          <button className="btn btn-primary btn-block">Subscribe</button>
+        </div>
+      </div>
+    </div>
   );
 }
