d1a9ce4a7b2e2bb99d8790ed21d9ae7b0d37d1c9
isakgranqvist2021 <isakwebdev@gmail.com>
2025-11-04 13:46:42 +0100

    Remove files

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4724f128ca4532977b54e6c0e1a18838e78ebfe8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8750824d8ce46bc2083cd6ff7159844012bfe149")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
