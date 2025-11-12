78bc0c4dc1953231a4b4daeabb0778ec8c8535a0
isakgranqvist2021 <isakwebdev@gmail.com>
2025-11-12 17:15:36 +0100

    Remove test

--- app/models/form.test.ts
+++
@@ -1,8 +0,0 @@
-import { expect, test } from 'vitest';
-import { fileSchema } from './form';
-
-test('empty file', () => {
-  const parseResult = fileSchema.safeParse(new File([], ''));
-  console.log(parseResult);
-  expect(parseResult.error).toBeDefined();
-});
